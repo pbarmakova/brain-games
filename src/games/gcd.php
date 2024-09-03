6dd901e4e11c0e14f1381854b53e05e302d180a9
nikto365 <pbarmakova@gmail.com>
2024-09-03 12:46:29 +0300

    update gcd

--- src/games/gcd.php
+++ src/games/gcd.php
@@ -5,21 +5,31 @@ namespace BrainGames\Gcd;
 use function cli\line;
 use function cli\prompt;
 
+function gcdGame(int $a, int $b): int
+{
+    while ($b !== 0) {
+        $temp = $b;
+        $b = $a % $b;
+        $a = $temp;
+    }
+    return $a;
+}
+
 function gcd($name)
 {
     $flag = 0;
     line('Find the greatest common divisor of given numbers.');
-    while ($flag != 3) {
+    while ($flag < 3) {
         $numOne = rand(1, 100);
         $numTwo = rand(1, 100);
         line("Question: %d %d", $numOne, $numTwo);
         $answ = (int)prompt('Your answer ');
-        $correctNum = gmp_intval(gmp_gcd($numOne, $numTwo));
+        $correctNum = gcdGame($numOne, $numTwo);
         if ($answ === $correctNum) {
             line('Correct!');
             $flag++;
         } else {
-            line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answ, $correctNum);
+            line("'%d' is wrong answer ;(. Correct answer was '%d'.", $answ, $correctNum);
             line("Let's try again, %s!", $name);
             break;
         }
